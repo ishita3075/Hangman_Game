09422acc309b47a4e7ae924911f50ef72e632603
Ishita Katiyar <iishitakatiyar@gmail.com>
2025-05-23 23:18:16 +0530

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,18 @@
 import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class App {
     public static void main(String[] args) {
+        String url="jdbc:mysql://localhost:3306/Hangman";
+        String username = "root";
+        String password="root@1234";
+        try(Connection connection=DriverManager.getConnection(url,username,password)){
+            System.out.println("Connected to database");
+            System.out.println(connection);
+        }catch(SQLException e){
+            System.err.println("Connection failed");
+        }
         SwingUtilities.invokeLater(new Runnable(){
             @Override
             public void run() {
